8c67b38321bf3ef254119a9a93f397c2a8754831
fadhlil18 <fadhlilazfi@gmail.com>
2024-07-10 20:06:11 +0700

    cors app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const config = require("./config/config");
 const routes = require("./routes");
 
@@ -6,6 +7,7 @@ const app = express();
 const PORT = config.appPort;
 
 app.use(express.json());
+app.use(cors());
 app.use(
   express.urlencoded({
     extended: false,
