43d23b4e227d3ac8fdcd45ab9c427f5e24333d7b
fadhlil18 <fadhlilazfi@gmail.com>
2024-07-13 19:36:22 +0700

    change slice to split

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const authMiddleware = (req, res, next) => {
   try {
-    const token = req.headers.authorization.slice[1];
+    const token = req.headers.authorization.split(' ')[1];
 
     if (!token) {
       throw new Error("Invalid Token");
